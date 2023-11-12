524ae716891c6459c222f63c8d05fa3026a766a5
Shafia <111538877+sfk30@users.noreply.github.com>
2023-11-12 13:34:15 +0000

    Added image import to webpack config

--- src/initialload.js
+++ src/initialload.js
@@ -1,3 +1,5 @@
+import burgerPic from './images/burger.jpg';
+
 const initialLoad = function() {
     const content = document.getElementById('content')
 
@@ -8,8 +10,14 @@ const initialLoad = function() {
     heading.textContent = 'Burgers and fries'
     pageContent.appendChild(heading)
 
-    const burger = document.createElement('img')
-    burger.src = "https://images.pexels.com/photos/2119758/pexels-photo-2119758.jpeg?auto=compress&cs=tinysrgb&w=800"
+    // const burger = document.createElement('img')
+    // burger.src = "https://images.pexels.com/photos/2119758/pexels-photo-2119758.jpeg?auto=compress&cs=tinysrgb&w=800"
+    // pageContent.appendChild(burger)
+
+    const burger = new Image()
+    burger.src = burgerPic
+    burger.height = 350
+    burger.width = 500
     pageContent.appendChild(burger)
 
     const phrase = document.createElement('p')
